307b4b6122343728120ab3e60de00ccd9e8b6460
Waqar Habib <waqarhabib94@aggienetwork.com>
2019-04-13 11:18:50 -0500

    added firebase links, modified css styling

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+
 var config = {
     apiKey: "AIzaSyDVIAQhYygPg8BTU_S9L_8lREoDe5eIpkA",
     authDomain: "flaremail-5c49d.firebaseapp.com",
@@ -7,3 +8,65 @@ var config = {
     messagingSenderId: "139402945796"
 };
 firebase.initializeApp(config);
+
+var database = firebase.database();
+
+var firstName = "";
+var lastName = "";
+var cellNumber = "";
+var email = "";
+var jobTitle = "";
+var department = "";
+var company = "";
+var webSite = "";
+var officePhone = "";
+var officeFax = "";
+var addressOne = "";
+var addressTwo = "";
+var city = "";
+var state ="";
+var zip = "";
+
+// On click function for generatedCode.html
+$("#generateCodeBtn").on('click', function () {
+   //This button should navigate user to generatedCode.html
+    event.preventDefault();
+    firstName = $("#userFirstName").val().trim();
+    lastName = $("userLastName").val().trim();
+    cellNumber = $("#userCellPhone").val().trim();
+    email = $("#userEmailAddress").val().trim();
+    jobTitle = $("#userJobTitle").val().trim();
+    department = $("#userDepartment").val().trim();
+    company = $("#userCompanyName").val().trim();
+    webSite = $("#userWebsiteURL").val().trim();
+    officePhone = $("#userOfficePhone").val().trim();
+    officeFax = $("#userOfficeFax").val().trim();
+    addressOne = $("#inputAddress").val().trim();
+    addressTwo = $("#inputAddress2").val().trim();
+    city = $("#inputCity").val().trim();
+    state = $("#inputState").val().trim();
+    zip = $("#inputZip").val().trim();
+
+    database.ref().push({
+        firstName = firstName,
+        lastName = lastName,
+        cellNumber = cellNumber,
+        email = email,
+        jobTitle = jobTitle,
+        department = department,
+        company = company,
+        webSite = webSite,
+        officePhone = officePhone,
+        officeFax = officeFax,
+        addressOne = addressOne,
+        addressTwo = addressTwo,
+        city = city,
+        state = state,
+        zip = zip,
+        dateAdded: firebase.database.ServerValue.TIMESTAMP
+    });
+});
+
+database.ref().on("child_added", function(snapshot){
+    
+})
