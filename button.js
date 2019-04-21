36ac4a4c0411ceb2cf00ac2f99cc6620b0744951
unknown <pwhynotfilms@gmail.com>
2019-04-21 12:24:04 -0500

    copy text button

--- button.js
+++ button.js
@@ -1,7 +1,7 @@
-const generateCode = document.getElementById("generateCode");
+const generatedCode = document.getElementById("generatedCode");
 const btnCopy = document.getElementById("btnCopy");
 
-btnCopy.onclick = function() {
-  generateCode.select();
+btnCopy.onclick = function(text) {
+  generatedCode.select();
   document.execCommand("Copy");
 };
